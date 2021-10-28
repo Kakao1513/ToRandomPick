c65ab2b2179cd53fc197274e3538144834ff9ca6
KAKAO1513 <minj21@naver.com>
2021-10-28 17:20:39 +0900

    2021-10-28

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,12 +12,14 @@ class spliter{
 class Draw{
     protected String[] members, gifts, winner;
     private final Random rand = new Random();
+    private String[][] winnerGift;
     private int[] winnerIndex;
     Draw(String mem, String gi){
         this.members = new String[spliter.spliting(mem).length];
         this.gifts = new String[spliter.spliting(gi).length];
         this.winner = new String[gifts.length];
         this.winnerIndex = new int[gifts.length];
+        this.winnerGift = new String[2][members.length];//세로 2 가로 멤버 변수의 길이 만큼의 배열
         members=spliter.spliting(mem);
         gifts=spliter.spliting(gi);
     }
@@ -33,31 +35,55 @@ class Draw{
                 }
             }
         }
-        for (int i = 0; i < winner.length; i++) {//맴버의 길이 만큼 반복.
+        for (int i = 0; i < winner.length; i++) {//당첨자의 길이 만큼 반복.
             winner[i]=members[winnerIndex[i]];
+        }
+        setGifts();
+    }
+    private void setGifts(){
+
+        for(int i = 0, j = 0; i< members.length;i++){//members의 길이만큼 반복
+            try{
+                if(winnerIndex[j]==i){
+                    winnerGift[0][i]=members[i];
+                    winnerGift[1][i]=gifts[j++];
+                }   else{
+                    winnerGift[0][i]=members[i];
+                    winnerGift[1][i]="꽝";
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                e.getMessage();
+                winnerGift[0][i]=members[i];
+                winnerGift[1][i]="꽝";
+            }
         }
     }
     void showWinner(){
         System.out.println("\n-----당첨-----");
         for(int i = 0 ; i< winner.length; i++){
-            System.out.println(gifts[i]+":"+winner[i]);
+            System.out.println(winner[i]+":"+gifts[i]);
         }
     }
     void showfail(){
-        int k=0;
         System.out.println("\n-----컽------");
-        for (String member : members) {
-            if (k > winner.length - 1) {
-                k = winner.length - 1;
-            }
-            if (!member.equals(winner[k])) {
-                System.out.println(member);
-            } else {//멤버가 같은경우
-                k++;
+        for(int i = 0 ; i<members.length;i++){
+            if(winnerGift[1][i].equals("꽝")){
+                System.out.println(members[i]);
             }
         }
     }
+    void showSequence(){
+        System.out.println("----뽑기 중----");
+        for(int i = 0 ; i< members.length;i++){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(winnerGift[0][i]+":"+winnerGift[1][i]);
 
+        }
+    }
 }
 public class Main {
 
@@ -65,16 +91,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("****** To Random Pick Draw ******");
+        while(true) {
+            System.out.print("Members:");
+            String allMembers = sc.nextLine();
 
-        System.out.print("Members:");
-        String allMembers = sc.nextLine();
+            System.out.print("Gifts:");
+            String gift = sc.nextLine();
 
-        System.out.print("Gifts:");
-        String gift = sc.nextLine();
+            Draw draw = new Draw(allMembers, gift);
+            draw.winnerDraw();
+            draw.showSequence();
+            draw.showWinner();
+            draw.showfail();
 
-        Draw draw = new Draw(allMembers, gift);
-        draw.winnerDraw();
-        draw.showWinner();
-        draw.showfail();
+            System.out.println("continue? Stop?");
+            System.out.println("If you want to continue, please enter the 1, otherwise enter another key.");
+            System.out.print("Enter:");
+            char ent = sc.next().charAt(0);
+            sc.nextLine();
+            if(ent!='1'){
+                break;
+            }
+        }
+
     }
 }
